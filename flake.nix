287a63f4df549e00029fb025ab585f52a5b430ec
hugosenari <hugosenari@gmail.com>
2022-03-12 04:12:52 -0300

    chore: re index smnar

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+  description = ''Servicio Meteorologico Nacional Argentina API Client'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs."smnar-master".url = "path:./master";
+  inputs."smnar-master".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."smnar-master".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@inputs:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib"];
+  in lib.mkProjectOutput {
+    inherit self nixpkgs;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+    refs = builtins.removeAttrs inputs args;
+  };
+}
